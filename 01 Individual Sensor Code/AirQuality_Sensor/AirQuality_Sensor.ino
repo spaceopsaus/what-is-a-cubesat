57dbc15f3b843bbdf9ba3db30463751be63f6679
spaceozcmd <contact@spaceops.com.au>
2019-08-28 17:38:41 +1000

    Comments
    Adding more comments with description of what the sensor is doing.

--- 01 Individual Sensor Code/AirQuality_Sensor/AirQuality_Sensor.ino
+++ 01 Individual Sensor Code/AirQuality_Sensor/AirQuality_Sensor.ino
@@ -1,20 +1,31 @@
 /*
   AirQuality Demo V1.0.
   connect to A0 to start testing. it will needs about 20s to start 
+  Further information from http://wiki.seeedstudio.com/Grove-Air_Quality_Sensor_v1.3/
 * By: http://www.seeedstudio.com
 */
 #include"AirQuality.h"
 #include"Arduino.h"
+
 AirQuality airqualitysensor;
-int current_quality =-1;
+int current_quality = -1;
+
 void setup()
 {
+  //Be sure to adjust the baudrate in Serial Monitor to match 9600
     Serial.begin(9600);
-    airqualitysensor.init(14);
+    //This sensor uses analog input to provide a value
+    //Connect the sensor to port A0.
+    airqualitysensor.init(A0);
 }
 void loop()
 {
-	current_quality=airqualitysensor.slope();
+  //The sensor works with ranges to determine the quality of air
+  //The ranges are based on voltage readings from the board.
+  //These ranges can be found inside the AirQuality.cpp file starting from line 88.
+  //By blowing air over the sensor or walking it outdoors you can see the value change.
+  
+    current_quality=airqualitysensor.slope();
     if (current_quality >= 0)// if a valid data returned.
     {
         if (current_quality==0)
@@ -27,11 +38,11 @@ void loop()
             Serial.println("Fresh air");
     }
 }
+
 ISR(TIMER1_OVF_vect)
 {
 	if(airqualitysensor.counter==61)//set 2 seconds as a detected duty
 	{
-
 			airqualitysensor.last_vol=airqualitysensor.first_vol;
 			airqualitysensor.first_vol=analogRead(A0);
 			airqualitysensor.counter=0;
