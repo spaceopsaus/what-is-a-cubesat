0b053ace2b1dabf39253c2140c6a69938d27149c
spaceozcmd <contact@spaceops.com.au>
2019-08-28 17:46:10 +1000

    More info
    Adding more comments to described how the loudness sensor works.

--- 01 Individual Sensor Code/Loudness_Sensor/Loudness_sensor.ino
+++ 01 Individual Sensor Code/Loudness_Sensor/Loudness_sensor.ino
@@ -1,3 +1,7 @@
+//The loudness sensor from Seeedstudio
+//More info from http://wiki.seeedstudio.com/Grove-Loudness_Sensor/
+
+//variable to hold the voltage.
 int loudness;
 
 void setup()
@@ -7,7 +11,15 @@ void setup()
 
 void loop()
 {
-    loudness = analogRead(0);
+  //The loudness sensor is based on reading the voltage of the A0 channel.
+  //The louder the sound, the greater voltage is output from A0.
+
+  //When you blow over the sensor, you may get values above 600
+  //Amdbient office environment will be near 200.
+
+  //A dial on the board can help tune maximum values recieved.
+  
+    loudness = analogRead(0); //Using port A0 or analog zero. Connect the sensor to A0 as marked.
     Serial.println(loudness);
     delay(200);
 }
