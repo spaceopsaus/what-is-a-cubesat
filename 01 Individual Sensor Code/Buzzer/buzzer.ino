74a3c54681259a9ef8d430c67565250e8024baf5
spaceozcmd <contact@spaceops.com.au>
2019-08-28 16:49:57 +1000

    Buzzer clean up
    Fixing typos and added further descriptive comments.

--- 01 Individual Sensor Code/Buzzer/buzzer.ino
+++ 01 Individual Sensor Code/Buzzer/buzzer.ino
@@ -1,11 +1,12 @@
-int buzzerPin = 6;      // LED connected to digital pin 9
-unsigned char val = 128;         // variable to store the read value
+//Buzzer
+
+int buzzerPin = 6;               // Buzzer connected to digital pin 6
+unsigned char val = 128;         // variable to store loudness of buzzer 0 -> 255
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);  // sets the pin as output
 }
 
 void loop() {
-  
-  analogWrite(buzzerPin, val);
+  analogWrite(buzzerPin, val); //on a loop send a signal to the buzzer to produce a sound.
 }
