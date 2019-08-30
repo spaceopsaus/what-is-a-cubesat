7fee9673265181375e68f3147e9491861f50875e
spaceozcmd <contact@spaceops.com.au>
2019-08-30 11:41:51 +1000

    Updating comments
    Describing more on how the speaker works.

--- 01 Individual Sensor Code/Speaker/Speaker.ino
+++ 01 Individual Sensor Code/Speaker/Speaker.ino
@@ -1,7 +1,15 @@
 /*macro definition of Speaker pin*/
-#define SPEAKER 3
+//The speaker is able to output at a variety of sounds.
+//These sounds can vary from loudness and pitches.
+
+//You can supply different frequency signal to this module with Arduino. 
+//Arduino generates these signal via PWM or even digital write and delay.
+//Here we are going to show you how to generate these signals using delay(), the speaker sound bass 1~7.
+  
+#define SPEAKER 3 //For this code, the speaker will use digital pin 3
 
 int BassTab[]={1911,1702,1516,1431,1275,1136,1012};//bass 1~7
+
 
 void setup()
 {
